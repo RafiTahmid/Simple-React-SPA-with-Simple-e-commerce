ddd8436b38294b71015988cdad8e3f6836170c2d
RafiTahmid <rafi.tahmid019@gmail.com>
2022-09-25 23:15:49 +0600

    Header design added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@
 import './App.css';
 import Header from './components/Header/Header';
 
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
     </div>
   );
