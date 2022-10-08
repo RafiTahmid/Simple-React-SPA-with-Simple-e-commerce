11dddf3a97ffa11c9ec213e0c717aad1e851687d
RafiTahmid <rafi.tahmid019@gmail.com>
2022-10-08 20:49:08 +0600

    stored in local storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
 import Main from "./layouts/Main";
+import Shop from "./components/Shop/Shop";
+import Orders from "./components/Orders/Orders";
+import Inventory from "./components/Inventory/Inventory";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          loader: () => fetch("products.json"),
+          element: <Shop></Shop>,
+        },
+        {
+          path: "/orders",
+          loader: () => fetch("products.json"),
+          element: <Orders></Orders>,
+        },
+        {
+          path: "/inventory",
+          element: <Inventory></Inventory>,
+        },
+        {
+          path: "about",
+          element: <About></About>,
+        },
+      ],
     },
   ]);
 
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,46 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import Cart from '../Cart/Cart';
-import Product from '../Product/Product';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import "./Shop.css";
 
 const Shop = () => {
+  // To load the data
+  // const [products, setProducts] = useState([]);
+  const products = useLoaderData();
+  const [cart, setCart] = useState([]);
 
-    // To load the data
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+  // To set the data
+  // useEffect( () => {
+  //     fetch('products.json')
+  //     .then(res => res.json())
+  //     .then(data => setProducts(data))
+  // }, [])
 
-    // To set the data
-    useEffect( () => {
-        fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    }, [])
-
-    const handleAddToCart = (product) =>{
-        console.log(product);
-        const newCart = [...cart, product];
-        setCart(newCart);
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      console.log(addedProduct);
     }
+  }, []);
 
-    return (
-        <div className='shop-container'>
-            <div className="products-container">
-                {
-                    products.map(product => <Product
-                        key={product.id}
-                        // To show all the products
-                        product = {product}
-                        handleAddToCart = {handleAddToCart}
+  const handleAddToCart = (product) => {
+    console.log(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
+    addToDb(product.id);
+  };
 
-                                            ></Product>)
-                }
-            </div>
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            // To show all the products
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
 
-           <div className="cart-container">
-                <Cart cart={cart}></Cart>
-            </div>
-        </div>
-    );
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
